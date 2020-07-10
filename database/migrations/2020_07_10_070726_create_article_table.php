8f12bc40b6f3865a53952b8a7313c29d9cc08dc6
Irfan Jauhari <kangrykong123@gmail.com>
2020-07-10 15:51:02 +0700

    initial

---
+++ database/migrations/2020_07_10_070726_create_article_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->unique();
+            $table->string('content');
+            // user
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('category_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->on('users')
+                ->references('id');
+            $table->foreign('category_id')
+                ->on('categories')
+                ->references('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
